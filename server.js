1646d206d07ab893195f528995bb47df2d5a0b65
tradalhariri <trad.alhariri@gmail.com>
2021-08-24 23:34:38 +0300

    add end points for adding and deleting book

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const cors = require('cors');
 require('dotenv').config();
 const server = express();
 server.use(cors());
+server.use(express.json());
 const bookModel = require('./modules/book-model');
 const PORT = process.env.PORT;
-
-const booksData = require('./constants/dummy-data')
 
 server.get('/', handleMainRoute);
 
@@ -18,7 +17,42 @@ function handleMainRoute(req,res){
 
 
 server.get('/books',getBooksByOwnerEmail);
+server.post('/books',addBookForOwnerEmail);
+server.delete('/books/:bookID',deleteBookForOwnerEmail);
 
+async function deleteBookForOwnerEmail(req,res){
+    const bookID = req.params.bookID;
+    const ownerName = req.query.ownerName;
+    bookModel.deleteOne({_id:bookID},(error,deletedBook)=>{
+        if(error) {
+            console.log('error in deleteing the book')
+        } else {
+            console.log('book has been deleted', deletedBook)
+            bookModel.find({ email:ownerName }, function (err, bookData) {
+                if (err) {
+                    console.log('error in getting the books')
+                } else {
+                    res.send(bookData)
+                }
+            })
+        }
+    })
+
+
+}
+
+async function addBookForOwnerEmail(req,res){
+const {ownerEmail,bookDescription,bookImageUrl,bookName} = req.body; 
+await bookModel.create({email:ownerEmail,description:bookDescription,image:bookImageUrl,title:bookName});
+bookModel.find({email:ownerEmail},(err,bookData)=>{
+    if(err){
+        res.status(500).send(err)
+    }else{
+        res.status(200).send(bookData);
+    }
+})
+
+}
 function getBooksByOwnerEmail(req,res){
     let ownerEmail = req.query.ownerEmail;
     bookModel.find({email:ownerEmail},function(err,bookData){
@@ -30,17 +64,6 @@ function getBooksByOwnerEmail(req,res){
 
     })
 }
-
-
-function fillBestBooksWithData(){
-
-booksData.map(book=>{
-    let myBook = new bookModel({title:book.title,description:book.description,image:book.image,email:book.email})
-    myBook.save();
-});
-}
-
-// fillBestBooksWithData();
 
 
 server.listen(PORT, () => console.log(`Server listening ${PORT}`));
